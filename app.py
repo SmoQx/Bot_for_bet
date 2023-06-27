5c59001e33831122f4abf66a89e3a32bd5b6d05a
SmoQ <smoqxpl@gmail.com>
2023-06-27 16:31:59 +0200

    edited what is imported from data about the teams

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 import csv
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def display_csv():
     return render_template('index.html')
+
+
+@app.route('/wynik', methods=['POST'])
+def redirect_page():
+    return redirect(url_for('another_page'))
+
+
+@app.route('/wynik')
+def another_page():
+    return render_template('/wynik_mecz.html')
 
 
 if __name__ == '__main__':
