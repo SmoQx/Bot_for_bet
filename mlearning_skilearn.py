a906535b61a35e577adb4505268d256a83c90cef
SmoQ <smoqxpl@gmail.com>
2023-06-28 15:57:03 +0200

    trying to change the algorythm

--- mlearning_skilearn.py
+++ mlearning_skilearn.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from data_reader import game_played, championship_scores
 import numpy as np
 
@@ -14,7 +16,9 @@ def open_championship_data(path):
 
 def calculate_likelihood(team1, team2):
     path_to_championship_data = ["football_data0.csv", "football_data1.csv"]
-    team_stats_df = open_championship_data(path_to_championship_data[0])
+    merger = pd.concat([open_championship_data(path_to_championship_data[0]),
+                        open_championship_data(path_to_championship_data[1])])
+    team_stats_df = merger
     game_scores_df = open_game_data()
     # Step 1: Input team names
     team1 = team1.strip()
@@ -24,35 +28,44 @@ def calculate_likelihood(team1, team2):
     team1_stats = team_stats_df.loc[team_stats_df['Team'] == team1]
     team2_stats = team_stats_df.loc[team_stats_df['Team'] == team2]
 
+    game_scores_df[['team1_score', 'team2_score']] = game_scores_df['score'].str.split(' - ', expand=True)
+    game_scores_df = game_scores_df.drop('score', axis=1)
+    print(game_scores_df)
     # Step 3: Calculate likelihood scores
     try:
         team1_goals_scored = game_scores_df[(game_scores_df['team1'] == team1) &
-                                            (game_scores_df['team2'] == team2)]['score'].apply(lambda x: int(x.split('-')[0].strip()))
+                                            (game_scores_df['team2'] == team2)]['team1_score'].apply(lambda x: int(x.strip()))
         team1_goals_conceded = game_scores_df[(game_scores_df['team1'] == team2) &
-                                              (game_scores_df['team2'] == team1)]['score'].apply(lambda x: int(x.split('-')[1].strip()))
-
+                                              (game_scores_df['team2'] == team1)]['team2_score'].apply(lambda x: int(x.strip()))
+        print(game_scores_df[(game_scores_df['team1_score'] != 0) & (game_scores_df['team1'] == team1) & (game_scores_df['team2'] == team2)])
+            #print(game_scores_df['team1_score'])
         team2_goals_scored = game_scores_df[(game_scores_df['team1'] == team2) &
-                                            (game_scores_df['team2'] == team1)]['score'].apply(lambda x: int(x.split('-')[0].strip()))
+                                            (game_scores_df['team2'] == team1)]['team2_score'].apply(lambda x: int(x.strip()))
         team2_goals_conceded = game_scores_df[(game_scores_df['team1'] == team1) &
-                                              (game_scores_df['team2'] == team2)]['score'].apply(lambda x: int(x.split('-')[1].strip()))
+                                              (game_scores_df['team2'] == team2)]['team1_score'].apply(lambda x: int(x.strip()))
 
         team1_likelihood = (
             team1_stats['W'].values[0] * team1_stats['GF'].values[0] * team2_stats['L'].values[0] *
-            team2_stats['GA'].values[0] * team1_goals_scored.mean() * team2_goals_conceded.mean() /
+            team2_stats['GA'].values[0] * team1_goals_scored.mean() /
             (team1_stats['W'].values[0] * team1_stats['GF'].values[0] * team2_stats['L'].values[0] *
-             team2_stats['GA'].values[0] * team1_goals_scored.mean() * team2_goals_conceded.mean() +
+             team2_stats['GA'].values[0] * team1_goals_scored.mean() +
              team2_stats['W'].values[0] * team2_stats['GF'].values[0] * team1_stats['L'].values[0] *
-             team1_stats['GA'].values[0] * team2_goals_scored.mean() * team1_goals_conceded.mean())
+             team1_stats['GA'].values[0] * team2_goals_scored.mean() * team1_goals_conceded.mean() +
+             team1_stats['FTS'].values[0])
         )
         team2_likelihood = (
             team2_stats['W'].values[0] * team2_stats['GF'].values[0] * team1_stats['L'].values[0] *
-            team1_stats['GA'].values[0] * team2_goals_scored.mean() * team1_goals_conceded.mean() /
+            team1_stats['GA'].values[0] * team2_goals_scored.mean() /
             (team2_stats['W'].values[0] * team2_stats['GF'].values[0] * team1_stats['L'].values[0] *
-             team1_stats['GA'].values[0] * team2_goals_scored.mean() * team1_goals_conceded.mean() +
+             team1_stats['GA'].values[0] * team2_goals_scored.mean() +
              team1_stats['W'].values[0] * team1_stats['GF'].values[0] * team2_stats['L'].values[0] *
-             team2_stats['GA'].values[0] * team1_goals_scored.mean() * team2_goals_conceded.mean())
+             team2_stats['GA'].values[0] * team1_goals_scored.mean() * team2_goals_conceded.mean() +
+             team1_stats['FTS'].values[0])
         )
-        print(team1_stats, team2_stats, team1_likelihood, team2_likelihood, team1_goals_scored, team2_goals_scored)
+
+        print(team1_stats, '\n', team2_stats, '\n',
+              team1_goals_scored, team2_goals_scored)
+
         return team1_likelihood * 100, team2_likelihood * 100
 
     except IndexError:
@@ -60,4 +73,10 @@ def calculate_likelihood(team1, team2):
 
 
 if __name__ == '__main__':
-    open_game_data()
+    team1 = "FC Kobenhavn"
+    team2 = "Manchester City"
+    game_scores_df = open_game_data()
+    print(calculate_likelihood(team1, team2))
+
+
+
